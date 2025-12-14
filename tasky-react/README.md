ffa20b7c978039af23f0fe7b90fda9aae527e396
lillianfitzpatrick <20102189@mail.wit.ie>
2025-12-14 20:32:01 +0000

    Authentication front-end set up

---
+++ tasky-react/README.md
@@ -0,0 +1,3 @@
+# react-basic-labs
+
+Labs for Web App Development Weeks 1 - 3
---
+++ tasky-react/src/components/Task.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const Task = (props) => {
+    
+          return (
+          <div className="card" style={{backgroundColor: props.done ? 'lightgrey' : '#5bb4c4'}}>
+
+            <p className="title">{props.title}</p>
+            <p>Due: {props.deadline}</p>
+            <p className="description">{props.description}</p>
+            
+
+            <div
+        className={`priorityBadge ${
+          props.priority === 'High'
+            ? 'priorityHigh'
+            : props.priority === 'Medium'
+            ? 'priorityMedium'
+            : 'priorityLow'
+        }`}
+      >
+        {props.priority}
+      </div>
+
+            <button onClick={props.markDone} className='doneButton'>Done</button>
+            <button className='deleteButton' onClick={props.deleteTask}>Delete</button>
+
+
+
+        </div>
+    )
+
+
+}
+
+
+export default Task;
